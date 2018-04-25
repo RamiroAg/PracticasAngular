541ecade004eb70cd3967805ab388aeb5341cef4
RamiroAg <ramiro.a.gutierrez@gmail.com>
2018-04-25 00:18:25 -0300

    Add project files.

---
+++ BikeRental/scripts/app/app.js
@@ -0,0 +1,23 @@
+﻿var myAppModule = angular.module('myApp', ['ngRoute',
+    'ui.bootstrap']);
+myAppModule.config([
+    '$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl:
+                    'scripts/app/views/default.html'
+            })
+            .when('/bicycles', {
+                templateUrl:
+                    'scripts/app/views/bicyclesIndex.html'
+            })
+            .when('/customers', {
+                templateUrl:
+                    'scripts/app/views/customersIndex.html'
+            })
+            .when('/rentals', {
+                templateUrl:
+                    'scripts/app/views/rentalsIndex.html'
+            });
+    }
+]);
