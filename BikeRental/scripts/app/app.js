9aa836e8da04b13417314aea557ee9e704119df7
RamiroAg <ramiro.a.gutierrez@gmail.com>
2018-04-26 23:32:50 -0300

    Se avanzó con el capítulo 2

--- BikeRental/scripts/app/app.js
+++ BikeRental/scripts/app/app.js
@@ -1,23 +1,15 @@
-﻿var myAppModule = angular.module('myApp', ['ngRoute',
-    'ui.bootstrap']);
-myAppModule.config([
-    '$routeProvider', function ($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl:
-                    'scripts/app/views/default.html'
-            })
-            .when('/bicycles', {
-                templateUrl:
-                    'scripts/app/views/bicyclesIndex.html'
-            })
-            .when('/customers', {
-                templateUrl:
-                    'scripts/app/views/customersIndex.html'
-            })
-            .when('/rentals', {
-                templateUrl:
-                    'scripts/app/views/rentalsIndex.html'
-            });
-    }
-]);
+﻿(function () {
+    "use strict";
+
+    var myAppModule = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
+    myAppModule.config([
+        '$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .when('/', {templateUrl:'scripts/app/views/default.html'})
+                .when('/bicycles', {templateUrl:'scripts/app/views/bicyclesIndex.html'})
+                .when('/customers', {templateUrl:'scripts/app/views/customersIndex.html'})
+                .when('/rentals', {templateUrl:'scripts/app/views/rentalsIndex.html'})                
+                .when('/bicycles/new', {templateUrl:'Scripts/app/views/bicyclesNew.html', controller:'BicycleController'});
+        }
+    ]);
+})();
